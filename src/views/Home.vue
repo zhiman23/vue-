fff612e8c0684ecfb4aa63e75f73ec69cbbc8cfe
yxy <2519675951@qq.com>
2020-12-03 22:15:25 +0800

    后台菜单的基本架构

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,73 @@
 <template>
-  <div>首页</div>
+  <el-container class="container">
+    <el-aside width="200px">
+      <h2>头条</h2>
+      <el-menu
+        class="el-menu-vertical-demo"
+        background-color="#333"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">文章列表</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">发表文章</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container class="container">
+      <el-header>头部内容</el-header>
+      <el-main class="main">主要内容</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  height: 100vh;
+}
+.el-menu {
+  border-right: none;
+}
+.el-header,
+.el-footer {
+  background-color: #333;
+  color: white;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #000000;
+  color: white;
+  text-align: center;
+  line-height: 100px;
+}
+
+.main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 </style>
