b8d2e37cfe50c6d0d1660adba36782915c182c8c
yxy <2519675951@qq.com>
2020-12-04 22:26:33 +0800

    添加拦截器

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://liangwei.tech:3000'
 Vue.prototype.$axios = axios
 
+axios.interceptors.request.use(config => {
+  if (!config.headers.Authorization && localStorage.getItem('token')) {
+    config.headers.Authorization = localStorage.getItem('token')
+  }
+  return config
+})
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
